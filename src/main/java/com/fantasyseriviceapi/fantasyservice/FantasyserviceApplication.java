8c7f4b184ee4a5de71b5bd377569f98875f7fe9b
Dan Sullivan <drsullivan13@gmail.com>
2019-02-18 17:48:23 -0500

    Inital Commit

---
+++ src/main/java/com/fantasyseriviceapi/fantasyservice/FantasyserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fantasyseriviceapi.fantasyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyserviceApplication.class, args);
+	}
+
+}
